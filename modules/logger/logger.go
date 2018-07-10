338f3c548243c9459b0902fe8d20f3fea7074849
徐志强 <xuzhiqiang@innotechx.com>
2018-07-10 20:42:09 +0800

    add time to log

--- modules/logger/logger.go
+++ modules/logger/logger.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"os"
 	"qpush/modules/config"
+	"time"
+)
+
+const (
+	// SEP for log sperator
+	SEP = " : "
 )
 
 // Info prints to stdout
 func Info(msg ...interface{}) {
+	fmt.Print(time.Now().String(), SEP)
 	fmt.Println(msg...)
 }
 
 // Error prints to stderr
 func Error(msg ...interface{}) {
+	fmt.Fprint(os.Stderr, time.Now().String(), SEP)
 	fmt.Fprintln(os.Stderr, msg...)
 }
 
@@ -25,5 +33,6 @@ func Debug(msg ...interface{}) {
 		return
 	}
 
+	fmt.Fprint(os.Stderr, time.Now().String(), SEP)
 	fmt.Fprintln(os.Stderr, msg...)
 }
