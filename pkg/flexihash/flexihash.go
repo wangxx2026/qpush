ee8871a732238c2484e605f023b8a1b73f5698c8
徐志强 <xuzhiqiang@innotechx.com>
2018-08-08 14:36:27 +0800

    add flexihash

--- pkg/flexihash/flexihash.go
+++ pkg/flexihash/flexihash.go
@@ -8,39 +8,56 @@ import (
 
 // Node is a member of the hash ring
 type Node struct {
-	Weight int
+	Weight uint32
 	Thing  interface{}
 }
 
-// GetNode gets a node from the ring
-func GetNode(key []byte, nodes []Node) *Node {
-	if len(nodes) == 0 {
-		panic("empty nodes")
+// FlexiHash for flexihash
+type FlexiHash struct {
+	nodes       []Node
+	totalWeight uint32
+}
+
+// NewFlexiHash returns a flexihash instance
+func NewFlexiHash(things []interface{}, weights []uint32) *FlexiHash {
+	if len(things) == 0 {
+		panic("empty")
 	}
 
-	totalWeight := 0
-	for i := range nodes {
-		totalWeight += nodes[i].Weight
-	}
-	targetWeight := crc32.ChecksumIEEE(key) % totalWeight
-	totalWeight = 0
-	for i := range nodes {
-		if totalWeight+nodes[i].Weight > targetWeight {
-			return &nodes[i]
+	if weights == nil {
+		weights = make([]uint32, len(things))
+		for idx := range weights {
+			weights[idx] = 1
 		}
-		totalWeight += n.Weight
+	}
+	nodes := make([]Node, len(things))
+	totalWeight := uint32(0)
+	for idx := range things {
+		nodes[idx] = Node{Weight: weights[idx], Thing: things[idx]}
+		totalWeight += weights[idx]
+	}
+
+	return &FlexiHash{nodes: nodes, totalWeight: totalWeight}
+}
+
+// getNode gets a node from the ring
+func (f *FlexiHash) getNode(key []byte) *Node {
+
+	targetWeight := crc32.ChecksumIEEE(key) % f.totalWeight
+	sumWeight := uint32(0)
+	for i := range f.nodes {
+		if sumWeight+f.nodes[i].Weight > targetWeight {
+			return &f.nodes[i]
+		}
+		sumWeight += f.nodes[i].Weight
 	}
 
 	// should never happen
 	return nil
 }
 
-// GetThing get thing from equal weighted things
-func GetThing(key []byte, things []interface{}) interface{} {
-	nodes := make([]Node, len(things))
-	for i: range things {
-		nodes[i] = Node{Weight: 1, Thing: things[i]}
-	}
-	node := getNode(key, nodes)
+// Get a thing by flexihash
+func (f *FlexiHash) Get(key []byte) interface{} {
+	node := f.getNode(key)
 	return node.Thing
 }
