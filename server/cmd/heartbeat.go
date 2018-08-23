b5bed5a10e9ee79c62b2ff83e990e8c3f0db2710
徐志强 <xuzhiqiang@innotechx.com>
2018-08-23 11:13:57 +0800

    add log

--- server/cmd/heartbeat.go
+++ server/cmd/heartbeat.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"qpush/pkg/logger"
 	"qpush/server"
+	"unsafe"
 
 	"github.com/zhiqiangxu/qrpc"
 )
@@ -13,11 +14,15 @@ type HeartBeatCmd struct {
 
 // ServeQRPC implements qrpc.Handler
 func (cmd *HeartBeatCmd) ServeQRPC(writer qrpc.FrameWriter, frame *qrpc.RequestFrame) {
-	logger.Debug("HeartBeatCmd called")
+	ci := frame.ConnectionInfo()
+	serveconn := ci.SC
+	mem := unsafe.Pointer(serveconn)
+
+	logger.Debug(mem, "HeartBeatCmd called")
 	writer.StartWrite(frame.RequestID, server.HeartBeatRespCmd, 0)
 	err := writer.EndWrite()
 	if err != nil {
-		logger.Error("HeartBeatCmd", err)
+		logger.Error(mem, "HeartBeatCmd", err)
 	}
 
 }
