6e4c5cfcc7322ea5c6e1260c771d91710644c13a
徐志强 <xuzhiqiang@innotechx.com>
2018-09-18 11:04:18 +0800

    mod push

--- app/client/agent/cmd/push.go
+++ app/client/agent/cmd/push.go
@@ -5,23 +5,25 @@ import (
 	"qpush/client"
 	"qpush/pkg/logger"
 	"qpush/server"
+	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/zhiqiangxu/qrpc"
 )
 
 var pushCmd = &cobra.Command{
-	Use:   "push [internal address] [msg id] [title] [content]",
+	Use:   "push [internal address] [app id] [msg id] [title] [content]",
 	Short: "connect to [internal address] and send push cmd",
 	Args:  cobra.MinimumNArgs(4),
 	Run: func(cmd *cobra.Command, args []string) {
 		internalAddress := args[0]
-		msgID := args[1]
-		title := args[2]
-		content := args[3]
+		appID, _ := strconv.Atoi(args[1])
+		msgID := args[2]
+		title := args[3]
+		content := args[4]
 
 		api := client.NewAPI([]string{internalAddress}, qrpc.ConnectionConfig{}, nil)
-		pushCmd := client.PushCmd{Msg: client.Msg{MsgID: msgID, Title: title, Content: content}}
+		pushCmd := client.PushCmd{AppID: appID, Msg: client.Msg{MsgID: msgID, Title: title, Content: content}}
 		frame, err := api.CallForFrame(context.Background(), server.PushCmd, pushCmd)
 		if err != nil {
 			panic(err)
