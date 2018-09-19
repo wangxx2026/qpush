5619a6647ea78c1827b1ceb782683ae4e314ba2a
徐志强 <xuzhiqiang@innotechx.com>
2018-09-19 17:28:41 +0800

    log for login cmd

--- server/cmd/login.go
+++ server/cmd/login.go
@@ -102,7 +102,7 @@ func (cmd *LoginCmd) ServeQRPC(writer qrpc.FrameWriter, frame *qrpc.RequestFrame
 		return
 	}
 	if result.Code != 0 {
-		logger.Error(mem, "login failed", string(resp))
+		logger.Error(mem, "login failed", string(resp), string(frame.Payload))
 		frame.Close()
 		return
 	}
