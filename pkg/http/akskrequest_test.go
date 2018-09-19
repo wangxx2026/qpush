7e267427fb8449e2153e37c12ab7fc324487c362
徐志强 <xuzhiqiang@innotechx.com>
2018-09-19 13:04:37 +0800

    add test

--- pkg/http/akskrequest_test.go
+++ pkg/http/akskrequest_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"qpush/pkg/config"
@@ -20,4 +21,13 @@ func TestAksk(t *testing.T) {
 	resp, err := DoAkSkRequest("POST", "/v1/pushaksk/offlinemsg", data)
 
 	fmt.Println(string(resp), err)
+}
+
+func TestAuthorization(t *testing.T) {
+	os.Chdir("/Users/xuzhiqiang/.gvm/pkgsets/go1.10/global/src/qpush")
+	config.Load("prod")
+
+	data := map[string]interface{}{"app_id": 1001, "app_key": "UwMTA1Nw", "guid": "c04792e2-97cf-4371-b547-cccd4fdac210"}
+	bytes, _ := json.Marshal(data)
+	fmt.Println(getAuthorization("POST", "/v1/pushaksk/offlinemsg", "application/json", bytes))
 }
